5e9720818459ba551dbd5571981fe8d726777278
Andrew Brown <andrew.brown@shopify.com>
2016-08-29 16:22:20 -0400

    support ssl

--- producer.rb
+++ producer.rb
@@ -8,6 +8,9 @@ kafka = Kafka.new(
   seed_brokers: ["kafka+ssl://ec2-52-207-103-240.compute-1.amazonaws.com:9096", "kafka+ssl://ec2-54-87-146-57.compute-1.amazonaws.com:9096", "kafka+ssl://ec2-54-87-148-197.compute-1.amazonaws.com:9096"],
   # Set an optional client id in order to identify the client to Kafka:
   client_id: "andrewjamesbrown",
+  ssl_ca_cert: ENV['KAFKA_TRUSTED_CERT'],
+  ssl_client_cert: ENV['KAFKA_CLIENT_CERT'],
+  ssl_client_cert_key: ENV['KAFKA_CLIENT_CERT_KEY'],
   )
 
 while true
