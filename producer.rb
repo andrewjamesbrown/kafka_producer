6d06858fc808be0fa59e45a3e89b0387df8605af
Andrew Brown <andrew.brown@shopify.com>
2016-08-29 16:17:54 -0400

    msg

--- producer.rb
+++ producer.rb
@@ -5,7 +5,7 @@ require "kafka"
 kafka = Kafka.new(
   
   # At least one of these nodes must be available:
-  seed_brokers: ["kafka+ssl://ec2-52-207-103-240.compute-1.amazonaws.com:9096,kafka+ssl://ec2-54-87-146-57.compute-1.amazonaws.com:9096,kafka+ssl://ec2-54-87-148-197.compute-1.amazonaws.com:9096"]
+  seed_brokers: ["kafka+ssl://ec2-52-207-103-240.compute-1.amazonaws.com:9096,kafka+ssl://ec2-54-87-146-57.compute-1.amazonaws.com:9096,kafka+ssl://ec2-54-87-148-197.compute-1.amazonaws.com:9096"],
   # Set an optional client id in order to identify the client to Kafka:
   client_id: "andrewjamesbrown",
   )
